74263b0e64e84bc051467cf2dc1c0e8a9d5bbaff
Raphael Baskerville <raphael.baskerville@gmail.com>
2016-02-03 14:51:58 -0800

    updates db.js schemas

--- server/db.js
+++ server/db.js
@@ -26,7 +26,23 @@ db.knex.schema.hasTable('streetSweeping').then(function (exists) {
   if (!exists) {
     db.knex.schema.createTable('streetSweeping', function (streetSegment) {
      streetSegment.increments('id').primary();
-     streetSegment.string('', 255);
+     streetSegment.number('Rte', 255);
+     streetSegment.string('Street Name', 255);
+
+     // street address number.
+     streetSegment.number('Address', 255);
+     streetSegment.string('Day of', 255);
+     streetSegment.string('AM/PM', 255);
+     streetSegment.string('Side', 1);
+     streetSegment.string('From', 255);
+     streetSegment.string('To', 255);
+     streetSegment.string('Opt-', 255);
+
+     // NOTE: we will have to change this most likely
+     streetSegment.number('segmentStartXCoordinate', 255);
+     streetSegment.number('segmentStartYCoordinates', 255);
+     streetSegment.number('segmentEndXCoordinates', 255);
+     streetSegment.number('segmentEndYCoordinates', 255);
    }).then(function (table) {
      console.log('Created Table', table);
    });
