fd0f9ca252edde523841b388ff8e531b9ebb62ca
Daniel Kim <daniel.kim1116@gmail.com>
2016-02-08 19:54:56 -0800

    anoother one

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ var morgan = require('morgan');
 var env = require('node-env-file');
 
 //DATA BASE
-// var ParkingDB = require('./db/parking.js');
-// var User = require('./db/user.js');
+var ParkingDB = require('./db/parking.js');
+var User = require('./db/user.js');
 
 //LOGIN
 var jwt = require('jsonwebtoken');
@@ -16,9 +16,9 @@ var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var cookieParser = require('cookie-parser');
-// var assignTokenSignin = require('./routers/assignTokenSignin.js');
-// var assignTokenGoogle = require('./routers/assignTokenGoogle.js');
-// var verifyToken = require('./routers/verifyToken');
+var assignTokenSignin = require('./routers/assignTokenSignin.js');
+var assignTokenGoogle = require('./routers/assignTokenGoogle.js');
+var verifyToken = require('./routers/verifyToken');
 
 var port = process.env.PORT || 8080;
 
@@ -51,24 +51,24 @@ app.use(cookieParser());
 //Every request with the beginning endpoint of its assigned URL
 //gets ran through the subrouter first
 // app.use('/api', verifyToken);
-// app.use('/auth', assignTokenSignin);
-// app.use('/auth/google', assignTokenGoogle);
+app.use('/auth', assignTokenSignin);
+app.use('/auth/google', assignTokenGoogle);
 
 // using x,y Google Maps coordinates, find and return all the permit zones for that area
-// app.get('/zones/:xCoord/:yCoord', function (req, res) {
-//   console.log('received request for', req.params.xCoord, req.params.yCoord);
-//   ParkingDB.findPermitZones([req.params.xCoord, req.params.yCoord]).then(function (data) {
-//     res.status(200).send(data);
-//   });
-// });
-//
-// // Add new parking zones from the front end when a post request to /zones is made
-// // this should be an an admin only feature
-// app.post('/zones', function (req, res) {
-//   ParkingDB.savePermitZones(req.body).then(function (data) {  //function is in parking.js)
-//     res.status(201).send(data);
-//   });
-// });
+app.get('/zones/:xCoord/:yCoord', function (req, res) {
+  console.log('received request for', req.params.xCoord, req.params.yCoord);
+  ParkingDB.findPermitZones([req.params.xCoord, req.params.yCoord]).then(function (data) {
+    res.status(200).send(data);
+  });
+});
+
+// Add new parking zones from the front end when a post request to /zones is made
+// this should be an an admin only feature
+app.post('/zones', function (req, res) {
+  ParkingDB.savePermitZones(req.body).then(function (data) {  //function is in parking.js)
+    res.status(201).send(data);
+  });
+});
 
 /**
  * environment file for developing under a local server
@@ -76,94 +76,94 @@ app.use(cookieParser());
  */
 // env(__dirname + '/.env');
 
-// var GOOGLE_CLIENT_ID = process.env.GOOGLECLIENTID;
-// var GOOGLE_CLIENT_SECRET = process.env.GOOGLECLIENTSECRET;
-// var FACEBOOK_CLIENT_ID = process.env.FACEBOOKCLIENTID;
-// var FACEBOOK_CLIENT_SECRET = process.env.FACEBOOKCLIENTSECRET;
-// /**
-//  * Serializing user id to save the user's session
-//  */
-// passport.serializeUser(function (user, done) {
-//   if (user.id) {
-//     done(null, user.id);
-//   } else {
-//     done(null, user);
-//   }
-// });
-//
-// passport.deserializeUser(function (id, done) {
-//   /*MySQL query for User.findById(id, function(err, user) {
-//     done(err, user);
-//   });*/
-//   User.read({ id: id }, function (err, user) {
-//     console.log('err in deserializing', err);
-//     done(err, user);
-//   });
-// });
-// /**
-//  * Sign in with facebook
-//  */
-// passport.use(new FacebookStrategy({
-//   clientID: FACEBOOK_CLIENT_ID,
-//   clientSecret: FACEBOOK_CLIENT_SECRET,
-//   callbackURL: '/auth/facebook/callback',
-//   profileFields: ['email'],
-//   passReqToCallback: true,
-// }, function (req, accessToken, refreshToken, profile, done) {
-//   console.log('profile', profile);
-//   User.read({ facebookId: profile.id }).then(function (user) {
-//     if (user) {
-//       console.log('there is a user', user);
-//       return done(null, user);
-//     } else {
-//       console.log('it doesnt exist', user);
-//       User.create({ facebookId: profile.id }).then(function (model) {
-//         return done(null, user);
-//       });
-//     }
-//   });
-// }));
-// /**
-//  * Sign in with Google
-//  */
-// passport.use(new GoogleStrategy({
-//   clientID: GOOGLE_CLIENT_ID,
-//   clientSecret: GOOGLE_CLIENT_SECRET,
-//   callbackURL: '/auth/google/callback',
-//   passReqToCallback: true,
-// }, function (req, accessToken, refreshToken, profile, done) {
-//   console.log('profile', profile.emails[0].value);
-//   User.read({ googleId: profile.emails[0].value }).then(function (user) {
-//     console.log('Here is the user', user);
-//     if (user) {
-//       return done(null, user);
-//     } else {
-//       User.create({ googleId: profile.emails[0].value }).then(function (model) {
-//         return done(null, user);
-//       });
-//     }
-//   });
-// }));
-// /**
-//  * Redirect to Google Signin and grab user info
-//  */
-// app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
-// app.get('/auth/google/callback',
-//   passport.authenticate('google', { scope: 'profile email', failureRedirect: '/' }),
-//   function (req, res, next) {
-//     //req.user has the user id
-//   });
-// /**
-//  * Redirect to Facebook Signin
-//  *
-//  * NOTE:It redirects to homepage when user authenticates for the first time
-//  */
-// app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
-// app.get('/auth/facebook/callback',
-//   passport.authenticate('facebook', { failureRedirect: '/' }),
-//   function (req, res, next) {
-//     console.log('request user', req.user);
-//     res.send(200);
-//   });
+var GOOGLE_CLIENT_ID = process.env.GOOGLECLIENTID;
+var GOOGLE_CLIENT_SECRET = process.env.GOOGLECLIENTSECRET;
+var FACEBOOK_CLIENT_ID = process.env.FACEBOOKCLIENTID;
+var FACEBOOK_CLIENT_SECRET = process.env.FACEBOOKCLIENTSECRET;
+/**
+ * Serializing user id to save the user's session
+ */
+passport.serializeUser(function (user, done) {
+  if (user.id) {
+    done(null, user.id);
+  } else {
+    done(null, user);
+  }
+});
+
+passport.deserializeUser(function (id, done) {
+  /*MySQL query for User.findById(id, function(err, user) {
+    done(err, user);
+  });*/
+  User.read({ id: id }, function (err, user) {
+    console.log('err in deserializing', err);
+    done(err, user);
+  });
+});
+/**
+ * Sign in with facebook
+ */
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_CLIENT_ID,
+  clientSecret: FACEBOOK_CLIENT_SECRET,
+  callbackURL: '/auth/facebook/callback',
+  profileFields: ['email'],
+  passReqToCallback: true,
+}, function (req, accessToken, refreshToken, profile, done) {
+  console.log('profile', profile);
+  User.read({ facebookId: profile.id }).then(function (user) {
+    if (user) {
+      console.log('there is a user', user);
+      return done(null, user);
+    } else {
+      console.log('it doesnt exist', user);
+      User.create({ facebookId: profile.id }).then(function (model) {
+        return done(null, user);
+      });
+    }
+  });
+}));
+/**
+ * Sign in with Google
+ */
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback',
+  passReqToCallback: true,
+}, function (req, accessToken, refreshToken, profile, done) {
+  console.log('profile', profile.emails[0].value);
+  User.read({ googleId: profile.emails[0].value }).then(function (user) {
+    console.log('Here is the user', user);
+    if (user) {
+      return done(null, user);
+    } else {
+      User.create({ googleId: profile.emails[0].value }).then(function (model) {
+        return done(null, user);
+      });
+    }
+  });
+}));
+/**
+ * Redirect to Google Signin and grab user info
+ */
+app.get('/auth/google', passport.authenticate('google', { scope: 'profile email' }));
+app.get('/auth/google/callback',
+  passport.authenticate('google', { scope: 'profile email', failureRedirect: '/' }),
+  function (req, res, next) {
+    //req.user has the user id
+  });
+/**
+ * Redirect to Facebook Signin
+ *
+ * NOTE:It redirects to homepage when user authenticates for the first time
+ */
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  function (req, res, next) {
+    console.log('request user', req.user);
+    res.send(200);
+  });
 
 app.listen(port);
