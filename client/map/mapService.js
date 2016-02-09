c86b00c27ea9a33e863491936f7884f9b80238e1
Raphael Baskerville <raphael.baskerville@gmail.com>
2016-02-09 14:36:16 -0800

    change dat

--- client/map/mapService.js
+++ client/map/mapService.js
@@ -1,5 +1,5 @@
-//var SERVER_URL = 'https://spotz.herokuapp.com';
-var SERVER_URL = 'http://localhost:8080';
+var SERVER_URL = 'https://spotz.herokuapp.com';
+// var SERVER_URL = 'http://localhost:8080';
 
 angular.module('MapServices', ['AdminServices'])
 
