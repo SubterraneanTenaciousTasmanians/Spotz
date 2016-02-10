5a0e308b47da3b0015f4e59618def9d2dc1b7966
Daniel Kim <daniel.kim1116@gmail.com>
2016-02-09 16:02:21 -0800

    prep for deployment

--- server/routers/verifyToken.js
+++ server/routers/verifyToken.js
@@ -9,7 +9,7 @@ var ParkingDB = require('./../db/parking.js');
  * environment file for developing under a local server
  * comment out before deployment
  */
-env(__dirname + '/../.env');
+// env(__dirname + '/../.env');
 
 var JWT_SECRET = process.env.JWTSECRET;
 
