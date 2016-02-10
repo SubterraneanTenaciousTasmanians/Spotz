ed0ee170d6e852bc12bcfa17661f581fd03f7c1c
Daniel Kim <daniel.kim1116@gmail.com>
2016-02-10 11:09:39 -0800

    11th

--- server/routers/assignTokenSignin.js
+++ server/routers/assignTokenSignin.js
@@ -52,7 +52,62 @@ assignToken.post('/signup', function (req, res) {
     });
   });
 });
+/**
+ * Serializing user id to save the user's session
+ */
+passport.serializeUser(function (user, done) {
+  if (!user.id) {
+    done(null, user);
+  } else {
+    done(null, user.id);
+  }
+});
 
+passport.deserializeUser(function (id, done) {
+  /*MySQL query for User.findById(id, function(err, user) {
+    done(err, user);
+  });*/
+  User.read({ id: id }, function (err, user) {
+    done(err, user);
+  });
+});
+/**
+ * Sign in with facebook
+ */
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_CLIENT_ID,
+  clientSecret: FACEBOOK_CLIENT_SECRET,
+  callbackURL: '/auth/facebook/callback',
+  profileFields: ['email'],
+}, function (accessToken, refreshToken, profile, done) {
+  User.read({ facebookId: profile.id }).then(function (user) {
+    if (user) {
+      return done(null, user);
+    } else {
+      User.create({ facebookId: profile.id }).then(function (model) {
+        return done(null, user);
+      });
+    }
+  });
+}));
+/**
+ * Sign in with Google
+ */
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback',
+}, function (accessToken, refreshToken, profile, done) {
+  return User.read({ googleId: profile.emails[0].value }).then(function (user) {
+    if (user) {
+      return done(null, user);
+    } else {
+      User.create({ googleId: profile.emails[0].value }).then(function (user) {
+        return done(null, user);
+      });
+    }
+  });
+}));
 /**
  * Redirect to Google Signin and grab user info
  */
--- server/server.js
+++ server/server.js
@@ -15,8 +15,6 @@ var User = require('./db/controllers/user.js');
 
 //LOGIN
 var passport = require('passport');
-var FacebookStrategy = require('passport-facebook').Strategy;
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var cookieParser = require('cookie-parser');
 var assignTokenSignin = require('./routers/assignTokenSignin.js');
 var verifyToken = require('./routers/verifyToken.js');
@@ -43,66 +41,7 @@ app.use(bodyparser.json());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieParser());
-var GOOGLE_CLIENT_ID = process.env.GOOGLECLIENTID;
-var GOOGLE_CLIENT_SECRET = process.env.GOOGLECLIENTSECRET;
-var FACEBOOK_CLIENT_ID = process.env.FACEBOOKCLIENTID;
-var FACEBOOK_CLIENT_SECRET = process.env.FACEBOOKCLIENTSECRET;
-/**
- * Serializing user id to save the user's session
- */
-passport.serializeUser(function (user, done) {
-  if (!user.id) {
-    done(null, user);
-  } else {
-    done(null, user.id);
-  }
-});
 
-passport.deserializeUser(function (id, done) {
-  /*MySQL query for User.findById(id, function(err, user) {
-    done(err, user);
-  });*/
-  User.read({ id: id }, function (err, user) {
-    done(err, user);
-  });
-});
-/**
- * Sign in with facebook
- */
-passport.use(new FacebookStrategy({
-  clientID: FACEBOOK_CLIENT_ID,
-  clientSecret: FACEBOOK_CLIENT_SECRET,
-  callbackURL: '/auth/facebook/callback',
-  profileFields: ['email'],
-}, function (accessToken, refreshToken, profile, done) {
-  User.read({ facebookId: profile.id }).then(function (user) {
-    if (user) {
-      return done(null, user);
-    } else {
-      User.create({ facebookId: profile.id }).then(function (model) {
-        return done(null, user);
-      });
-    }
-  });
-}));
-/**
- * Sign in with Google
- */
-passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: 'https://spotz.herokuapp.com/auth/google/callback',
-}, function (accessToken, refreshToken, profile, done) {
-  return User.read({ googleId: profile.emails[0].value }).then(function (user) {
-    if (user) {
-      return done(null, user);
-    } else {
-      User.create({ googleId: profile.emails[0].value }).then(function (user) {
-        return done(null, user);
-      });
-    }
-  });
-}));
 /*
  *Subrouters
  */
