41ceb8bc4727c5d57d47f7a9bc05f469c724575e
Daniel Kim <daniel.kim1116@gmail.com>
2016-02-12 15:08:25 -0800

    about to start working on the donation modal

--- client/navBar/navCtrl.js
+++ client/navBar/navCtrl.js
@@ -1,7 +1,29 @@
 angular.module('spotz.nav', ['NavServices'])
 
 .controller('navCtrl', ['$scope', 'NavFactory', function ($scope, NavFactory) {
+  $scope.info = {};
+  $scope.transaction = {};
+  $scope.amount = 0;
+  $scope.showForm = true;
+  $scope.message;
+  $scope.paid = false;
 
   // nothing yet
+  $scope.handleStripe = function (status, response) {
+    console.log('MONEY', $scope.amount);
+    console.log('THIS ', $scope.info);
+    console.log('STATUSCODE ', status);
+    var clientToken = Stripe.card.createToken($scope.info, NavFactory.stripeCallback);
+    console.log('clientToken ', clientToken);
+    console.log('1st STRIPE RESPONSE', response);
+    console.log('PAID??? ', $scope.paid);
+  };
 },
-]);
+])
+
+.directive('donateModal', function () {
+  return {
+    restrict: 'E',
+    template: ''
+  }
+})
--- client/navBar/navService.js
+++ client/navBar/navService.js
@@ -4,6 +4,34 @@ angular.module('NavServices', [])
 
   var factory = {};
 
+  factory.stripeCallback = function (status, response) {
+    console.log('2nd STRIPE RESPONSE', response);
+    if (response.error) {
+      // there was an error. Fix it.
+      console.log('ERROR', response.error);
+    } else {
+      console.log('AMOUNT', $scope.amount);
+      $scope.transaction = {
+        token: response.id,
+        amount: $scope.amount,
+      };
+      $http.post('/donate', $scope.transaction).then(function (data) {
+        console.log('RESPOSNE FROM SERVER ', data);
+        if (data.status == 'OK') {
+          $scope.paid = true;
+          $scope.message = data.message;
+        } else {
+          $scope.paid = false;
+          $scope.message = data.message;
+        }
+
+        console.log('PAID IN FACTORY', $scope.paid);
+      }).catch(function (err) {
+        console.error('error', err);
+      });
+    }
+  };
+
   return factory;
 },
 ]);
