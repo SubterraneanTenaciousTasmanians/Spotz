30567b71121df678e5419957b61acbe66a57d60b
Daniel Kim <daniel.kim1116@gmail.com>
2016-02-13 15:13:55 -0800

    3rd test

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ var app = express();
 //CORS
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
