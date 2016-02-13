a54c7abe4fc2d02cb12db0bc530388f021ac489e
Daniel Kim <daniel.kim1116@gmail.com>
2016-02-12 16:08:22 -0800

    donation modal directive finished

--- client/navBar/navCtrl.js
+++ client/navBar/navCtrl.js
@@ -7,23 +7,40 @@ angular.module('spotz.nav', ['NavServices'])
   $scope.showForm = true;
   $scope.message;
   $scope.paid = false;
+  $scope.modalShown = false;
+  $scope.toggleModal = function () {
+    console.log('toggle modal');
+    $scope.modalShown = !$scope.modalShown;
+  };
+
+  $scope.stripeCallback = function (status, response) {
+    console.log('2nd STRIPE RESPONSE', response);
+    if (response.error) {
+      // there was an error. Fix it.
+      console.log('ERROR', response.error);
+    } else {
+      console.log('AMOUNT', $scope.amount);
+      $scope.transaction = {
+        token: response.id,
+        amount: $scope.amount,
+      };
+      NavFactory.requestToken($scope.transaction).then(function (response) {
+        console.log('RESPOSNE FROM FACTORY ', response);
+        $scope.paid = response.paid;
+        $scope.message = response.message;
+      });
+    }
+  };
 
   // nothing yet
   $scope.handleStripe = function (status, response) {
     console.log('MONEY', $scope.amount);
     console.log('THIS ', $scope.info);
     console.log('STATUSCODE ', status);
-    var clientToken = Stripe.card.createToken($scope.info, NavFactory.stripeCallback);
+    var clientToken = Stripe.card.createToken($scope.info, $scope.stripeCallback);
     console.log('clientToken ', clientToken);
     console.log('1st STRIPE RESPONSE', response);
     console.log('PAID??? ', $scope.paid);
   };
 },
-])
-
-.directive('donateModal', function () {
-  return {
-    restrict: 'E',
-    template: ''
-  }
-})
+]);
--- client/navBar/navService.js
+++ client/navBar/navService.js
@@ -4,32 +4,22 @@ angular.module('NavServices', [])
 
   var factory = {};
 
-  factory.stripeCallback = function (status, response) {
-    console.log('2nd STRIPE RESPONSE', response);
-    if (response.error) {
-      // there was an error. Fix it.
-      console.log('ERROR', response.error);
-    } else {
-      console.log('AMOUNT', $scope.amount);
-      $scope.transaction = {
-        token: response.id,
-        amount: $scope.amount,
-      };
-      $http.post('/donate', $scope.transaction).then(function (data) {
+  factory.requestToken = function (info) {
+    return $http.post('/donate', info).then(function (data) {
         console.log('RESPOSNE FROM SERVER ', data);
         if (data.status == 'OK') {
-          $scope.paid = true;
-          $scope.message = data.message;
+          return { paid: true,
+            message: data.message,
+          };
         } else {
-          $scope.paid = false;
-          $scope.message = data.message;
+          return { paid: false,
+            message: data.message,
+          };
         }
 
-        console.log('PAID IN FACTORY', $scope.paid);
       }).catch(function (err) {
         console.error('error', err);
       });
-    }
   };
 
   return factory;
