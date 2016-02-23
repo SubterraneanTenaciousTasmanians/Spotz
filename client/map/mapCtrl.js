0af70f2e6cb4fee8a75b639fea06a3a225ba0753
HarrisonA <radiov@gmail.com>
2016-02-22 20:39:24 -0800

    Fixed linter issue.  Map now displays a warning when max zoom out is reached.

--- client/map/mapCtrl.js
+++ client/map/mapCtrl.js
@@ -2,8 +2,10 @@
 
 angular.module('spotz.map', ['MapServices'])
 
-.controller('mapCtrl', ['$scope', '$rootScope', '$cookies', '$state', 'MapFactory', 'LoginFactory', function ($scope, $rootScope, $cookies, $state, MapFactory, LoginFactory) {
+.controller('mapCtrl', ['$scope', '$rootScope', '$cookies', '$state','MapFactory', 'LoginFactory', function ($scope, $rootScope, $cookies, $state, MapFactory, LoginFactory) {
   $scope.mapLoading = true;
+
+  $scope.maxZoomOut = false;
 
   $rootScope.$on('googleMapLoaded', function () {
     $scope.mapLoading = false;
@@ -16,6 +18,20 @@ angular.module('spotz.map', ['MapServices'])
   $rootScope.$on('mapLoaded', function () {
     $scope.mapLoading = false;
   });
+
+  $rootScope.$on('maxZoomOutReached', function () {
+    console.log('max zoom level reached.  Please use the search bar');
+    console.log("$scope.maxZoomOut right before setting to true", $scope.maxZoomOut)
+    // $scope.maxZoomOut = true;
+    $scope.maxZoomOut = true;
+    $scope.$apply();
+  });
+
+  $rootScope.$on('lessThanMaxZoomOut', function () {
+    $scope.maxZoomOut = false;
+    $scope.$apply();
+  });
+
 
   $scope.deleteRule = function (zoneId, ruleId) {
     console.log('deleting rule', ruleId, 'for', zoneId);
